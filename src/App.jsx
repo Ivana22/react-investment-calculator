0e6fb05aff29d0a7f3755513fd6102177aeb5ef0
ivanatrpkova <ivana.trpkova@codeflair.ch>
2023-12-01 11:24:01 +0100

    react-investment-calculator-exercise

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+import { useState } from "react";
+import { calculateInvestmentResults } from "./util/investment.js";
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 1200,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than 0!</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  );
+}
+
+export default App;
